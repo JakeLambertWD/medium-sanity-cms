a31500a4b426b9488d52d9ae2504aaf6de518fec
JakeLambertWD <jakelambert1@hotmail.co.uk>
2022-06-20 11:31:28 +0100

    Initial setup w/ Sanity CMS

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Next.js x Tailwind CSS x TypeScript
 
 npx create-next-app medium-app --example with-typescript
+
+# Sanity.io toolkit for Next.js
+
+npm i next-sanity
---
+++ sanity.js
@@ -0,0 +1,14 @@
+import { createImageUrlBuilder, createCurrentUserHook, createClient } from 'next-sanity';
+
+export const config = {
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	apiVersion: '2022-06-20',
+	useCdn: process.env.NODE_ENV === 'production'
+};
+
+// Setup the client for fetching data in the getProps page functions
+export const sanityClient = createClient(config);
+
+// Setup a helper function for generating Image URL's with only the asset reference data in your documents.
+export const urlFor = source => createImageUrlBuilder(config).image(source);
